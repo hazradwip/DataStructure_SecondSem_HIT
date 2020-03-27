c172138eb4480d7f7c69ab38bee5be45874bbd0d
amitm99 <manna.amit99@gmail.com>
2020-03-27 20:10:58 +0530

    Done

---
+++ Day-06/doubly_linked_list.c
@@ -0,0 +1,214 @@
+#include "doubly_linked_list.h"
+
+void display(Node *head)
+{
+    Node *p;
+    p = head;
+    while (p != NULL)
+    {
+        printf(" <->(%d)", p->data);
+        p = p->next;
+    }
+}
+
+int count(Node *head)
+{
+    Node *p;
+    int c = 0;
+
+    p = head;
+    while (p != NULL)
+    {
+        ++c;
+        p = p->next;
+    }
+
+    return c;
+}
+
+/*
+* Create Linked List
+*/
+
+Node *create(Node *head, int x)
+{
+    Node *p, *temp;
+
+    temp = (Node *)malloc(sizeof(Node));
+    temp->data = x;
+    temp->prev = NULL;
+    temp->next = NULL;
+
+    if (head == NULL)
+        head = temp;
+    else
+    {
+        p = head;
+        while (p->next != NULL)
+            p = p->next;
+        p->next = temp;
+        temp->prev = p;
+    }
+
+    return head;
+}
+
+/*
+* Add a Node to Linked List
+*/
+
+Node *insert_beg(Node *head, int x)
+{
+    Node *temp;
+
+    temp = (Node *)malloc(sizeof(Node));
+    temp->data = x;
+    temp->prev = NULL;
+    temp->next = head;
+
+    head = temp;
+
+    return head;
+}
+
+Node *insert_end(Node *head, int x)
+{
+    Node *p, *temp;
+
+    temp = (Node *)malloc(sizeof(Node));
+    temp->data = x;
+    temp->prev = NULL;
+    temp->next = NULL;
+
+    p = head;
+    while (p->next != NULL)
+        p = p->next;
+    p->next = temp;
+    temp->prev = p;
+
+    return head;
+}
+
+Node *insert_anyPos(Node *head, int pos, int x)
+{
+    if (pos == 1)
+        head = insert_beg(head, x);
+
+    else if (pos == (count(head) + 1))
+        head = insert_end(head, x);
+
+    else
+    {
+        Node *p, *temp;
+
+        p = head;
+        temp = (Node *)malloc(sizeof(Node));
+        temp->data = x;
+        temp->prev = NULL;
+        temp->next = NULL;
+
+        int c = 1;
+        while (p->next != NULL)
+        {
+            if (c == pos - 1)
+            {
+                temp->next = p->next;
+                p->next = temp;
+                temp->prev = p;
+            }
+            c++;
+            p = p->next;
+        }
+    }
+
+    return head;
+}
+
+/*
+* Delete a Node from Linked List
+*/
+
+Node *del_beg(Node *head)
+{
+    Node *p;
+
+    p = head;
+    if (p == NULL)
+        printf("\nLinked List is empty. No Node to delete.\n");
+    else
+    {
+        head = head->next;
+        head->prev = NULL;
+    }
+    printf("\n%d is Deleted.\n", p->data);
+    free(p);
+
+    return head;
+}
+
+Node *del_end(Node *head)
+{
+    Node *p, *q;
+    p = head;
+    if (head == NULL)
+        printf("\n No need to delete\n");
+
+    else if (p->next == NULL)
+    {
+        printf("\n%d is Deleted and there is no other Node to Delete.\n", p->data);
+        free(p);
+        head = NULL;
+    }
+
+    else
+    {
+        //q = p->next;
+        while (p->next != NULL)
+        {
+            //q = q->next;
+            p = p->next;
+        }
+        q = p;
+        p = p->prev;
+        p->next = NULL;
+        printf("\n%d is Deleted.\n", q->data);
+        free(q);
+    }
+
+    return head;
+}
+
+Node *del_anyPos(Node *head, int pos)
+{
+    if (head == NULL)
+        printf("No Node to delete\n");
+
+    else if (head->next == NULL)
+        head = del_beg(head);
+
+    else if (pos == count(head))
+        head = del_end(head);
+
+    else
+    {
+        Node *p, *q, *temp;
+        int c = 1;
+        p = head;
+        while (p->next != NULL && c < pos)
+        {
+            q = p;
+            p = p->next;
+            c++;
+        }
+
+        if (c == pos)
+        {
+            q->next = p->next;
+            p->next->prev = q;
+            printf("\n%d is Deleted.\n", p->data);
+            free(p);
+        }
+
+        return head;
+    }
+}
---
+++ Day-06/main_doubly.c
@@ -0,0 +1,101 @@
+#include "doubly_linked_list.h"
+
+int main()
+{
+    int ch, choice, n, i, data, pos;
+    char c = 'y';
+    Node *head = NULL;
+
+    do
+    {
+        printf("\n\n-------MENU-------\n");
+        printf("1. Create List\n");
+        printf("2. Insert\n");
+        printf("3. Delete\n");
+        printf("4. Display\n");
+        printf("Choose an Option : ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("\nEnter the no.of Nodes to create\n");
+            scanf("%d", &n);
+
+            for (i = 0; i < n; i++)
+            {
+                printf("\nEnter the value of the  Node : \n");
+                scanf("%d", &data);
+                head = create(head, data);
+            }
+
+            break;
+        case 2:
+            printf("\n1. Insert at Beginning\n");
+            printf("2. Insert at End\n");
+            printf("3. Insert at any Position\n");
+            printf("Enter choice : ");
+            scanf("%d", &choice);
+            switch (choice)
+            {
+            case 1:
+                printf("\nEnter value : ");
+                scanf("%d", &data);
+                head = insert_beg(head, data);
+                break;
+            case 2:
+                printf("\nEnter value : ");
+                scanf("%d", &data);
+                head = insert_end(head, data);
+                break;
+            case 3:
+                printf("\nEnter position to insert : ");
+                scanf("%d", &pos);
+                if (pos > count(head) + 1)
+                    printf("\nInsertion not possible, Enter a shorter range...");
+                else
+                    printf("\nEnter value : ");
+                scanf("%d", &data);
+                head = insert_anyPos(head, pos, data);
+                break;
+            default:
+                printf("\nInvalid choice\n");
+                break;
+            }
+            break;
+        case 3:
+            printf("\n1. Delete Beginning\n");
+            printf("2. Delete from End\n");
+            printf("3. Delete from any Position\n");
+            printf("\nEnter choice : ");
+            scanf("%d", &choice);
+            switch (choice)
+            {
+            case 1:
+                head = del_beg(head);
+                break;
+            case 2:
+                head = del_end(head);
+                break;
+            case 3:
+                printf("\nEnter the position to delete : ");
+                scanf("%d", &pos);
+                if (pos > count(head))
+                    printf("\nDeletion not possible, Enter a shorter range...");
+                else
+                    head = del_anyPos(head, pos);
+                break;
+            default:
+                printf("\nInvalid choice\n");
+                break;
+            }
+            break;
+        case 4:
+            display(head);
+            break;
+        default:
+            printf("\nInvalid choice\n");
+        }
+        printf("\nDo you want to continue ? (y/n) : ");
+        c = getche();
+    } while (c == 'y' || c == 'Y');
+}
